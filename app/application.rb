95c67be90869085dc160edde4c5afa2fba93a7e3
Alex Lee <hyunho98@gmail.com>
2020-02-18 00:40:44 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,13 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+
+    if Time.now[3] < 12
+      resp.write "Good Morning"
+    else
+      resp.write "Good Afternoon"
+    end
+  end
+
+end
